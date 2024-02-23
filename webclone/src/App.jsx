69df73e6fad9fca812102c3936e42a9ea658de44
satyarthsrivastava <satyarthsrivastava7@gmail.com>
2024-02-23 19:02:12 +0530

    Add project files.

---
+++ webclone/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Navbar from "./assets/Navbar.jsx"
+import Main from "./assets/Main.jsx"
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
---
+++ webclone/src/assets/Main.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+export default function main() {
+    return (
+        <div>
+            <h1>WebClone</h1>
+        </div>
+    )
+}
